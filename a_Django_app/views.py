9a2a96a0915c35d8d607abe57e4a9df634cea736
StephDay <128762810+StephDay@users.noreply.github.com>
2023-04-02 19:16:50 +1000

    chatbot import

--- a_Django_app/views.py
+++ a_Django_app/views.py
@@ -1,16 +1,15 @@
 import os
 import sys
-
+import requests
 from django.core.wsgi import get_wsgi_application
+from django.shortcuts import render
+from a_Django_app.models import Package
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'a_Django_project.settings')
 application = get_wsgi_application()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
-
-from django.shortcuts import render
-from a_Django_app.models import Package
 
 
 def home(request):
@@ -33,3 +32,19 @@ def packages(request):
 def package_detail(request, pk):
     package = Package.objects.get(pk=pk)
     return render(request, 'package_detail.html', {'package': package})
+
+
+def chatbot(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+
+        api_url = 'https://w60ancsmh5.execute-api.ap-southeast-2.amazonaws.com/Prod'
+        headers = {'Content-Type': 'application/json'}
+        data = {'message': message}
+
+        response = requests.post(api_url, headers=headers, json=data)
+        chatbot_response = response.json()['body']['message']
+
+        return render(request, 'chatbot.html', {'chatbot_response': chatbot_response})
+
+    return render(request, 'chatbot.html')
